3ed48d671faddd3e91f78475ff4f3fd68921e3bf
JohnDonavon <john.stephens95@gmail.com>
2023-09-14 10:30:31 -0700

    deserialization fixes

--- src/reference/reference.ts
+++ src/reference/reference.ts
@@ -9,18 +9,8 @@ export const webgpu_pippenger_msm = async (
   baseAffinePoints: Point[],
   scalars: bigint[]
 ) => {
-  const point = {
-    x: BigInt('2796670805570508460920584878396618987767121022598342527208237783066948667246'),
-    y: BigInt('8134280397689638111748378379571739274369602049665521098046934931245960532166'),
-    t: BigInt('3446088593515175914550487355059397868296219355049460558182099906777968652023'),
-    z: BigInt('1')
-  }
-  const tempArray = new Array(baseAffinePoints.length);
-  tempArray.fill(point);
-  baseAffinePoints = tempArray;
-  console.log('baseAffinePoints', baseAffinePoints);
-  console.log('scalars', scalars);
   const fieldMath = new FieldMath();
+  console.log('baseAffinePoints', baseAffinePoints);
   const pointsAsU32s = baseAffinePoints.map(point => fieldMath.createPoint(point.x, point.y, point.t, point.z));
   const scalarsAsU16s = Array.from(bigIntsToU16Array(scalars));
   return await pippinger_msm(pointsAsU32s, scalarsAsU16s, fieldMath);
@@ -40,6 +30,7 @@ export const wasm_compute_msm = async (
   baseAffinePoints: Point[],
   scalars: bigint[]
   ): Promise<{x: bigint, y: bigint}> => {
+  console.log('baseAffinePoints', baseAffinePoints);
   const aleo = await loadWasmModule();
   const groups = baseAffinePoints.map(point => point.x.toString() + 'group');
   const aleoScalars = scalars.map(scalar => scalar.toString() + 'scalar');
